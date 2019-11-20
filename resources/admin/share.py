3306763c4cef4e7814d3552263fb98aeb280204f
Percy Quezada <percy.098@hotmail.com>
2019-11-20 06:08:47 -0500

    PDF version 1.0

---
+++ resources/admin/share.py
@@ -0,0 +1,28 @@
+from app import db
+from models.share import Share
+from resources.admin.security import AuthRequiredResource
+from flask_restful import Resource
+from flask import request
+from sqlalchemy.exc import SQLAlchemyError
+import status
+
+class ShareListResource(AuthRequiredResource):
+	def get(self):
+		try:
+			shares = Share.query.filter_by(idLoan=65)
+			d = []
+			for share in shares:
+				e = share.toJson()
+				d.append(e)
+
+			return d, status.HTTP_200_OK
+
+		except SQLAlchemyError as e:
+			db.session.rollback()
+			response = {'error', str(e)}
+			return response, status.HTTP_400_BAD_REQUEST
+
+		except Exception as e:
+			db.session.rollback()
+			response = {'error', str(e)}
+			return response, status.HTTP_400_BAD_REQUEST
